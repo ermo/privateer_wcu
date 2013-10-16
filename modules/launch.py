a2b7b19fae5ce5bcb071d47e01a0a550d18a9d64
Rune Morling <ermo.vega-strike.org@spammesenseless.net>
2013-10-16 21:15:19 +0200

    Instrument and fix launch.py, such that it respects faction settings.
    Before this fix, non-blank ships would almost never be launched.
    
    Instead, blank ships would be selected, upgraded and launched, which
    is problematic insofar as the ship upgrade routine does not appear to
    respect class restrictions.
    
    After this fix, it is again possible to configure whether a faction
    should always launch non-blank ships or if there should be a certain
    chance to launch non-standard, upgraded .blank ships.
    
    It is worth noting that the ship upgrade routine was not changed
    in this commit.

--- modules/launch.py
+++ modules/launch.py
@@ -1,61 +1,79 @@
-from difficulty import usingDifficulty
+import VS
+import debug
+import dj_lib
+import ship_upgrades
+import sys
 import vsrandom
 import unit
-import ship_upgrades
-import VS
-import sys
-import dj_lib
+from difficulty import usingDifficulty
+
+
 def launch (fgname, faction, type, ai, nr_ships, nr_waves, vec, logo='',useani=1):
-#  print 'log'+ str( logo) + ' useani '+ str(useani)
-    diff=usingDifficulty()
+    debug.debug("fgname: %s, faction %s" % (fgname, faction))
+    debug.debug("type: %s" % (type))
+    debug.debug("ai: %s, nr_ships: %d, nr_waves: %d" % (ai, nr_ships, nr_waves))
+    debug.debug("vec: %s, logo: %s, useani: %d" % (vec, logo, useani))
+    #print 'log'+ str( logo) + ' useani '+ str(useani)
+    use_diff = usingDifficulty()
 #   if useani:
 #       VS.playAnimation ("warp.ani",vec,300.0)
     if isinstance(type, list) or isinstance(type, tuple):
+        debug.debug("inside isinstance conditional (type is list or tuple)")
         for t in type:
             if t.endswith(".blank"):
                 isblank = True
                 break
     else:
-        isblank = not type.endswith(".blank") == -1
-    if (not diff or not isblank):
+        isblank = type.endswith(".blank")
+    debug.debug("isblank = %s" % (isblank))
+    # originally, this was
+    #   if (not diff or not isblank)
+    # which effectively always ignored isblank, since diff is almost never false
+    if (not isblank):
+        debug.info("Launching non-blank ship(s)")
         ret = VS.launch (fgname,type,faction,"unit",ai,nr_ships,nr_waves,VS.SafeEntrancePoint (vec,40),logo)
         dj_lib.PlayMusik(0,dj_lib.HOSTILE_NEWLAUNCH_DISTANCE)
-        return ret
-    rsize=0.0
-    diffic = VS.GetDifficulty()
-    ret=VS.Unit()
-    for i in range(nr_ships):
-        mynew=VS.launch(fgname,type,faction,"unit",ai,1,nr_waves,VS.SafeEntrancePoint (vec,40),logo)
-        if (i==0):
-            ret = mynew
-            rsize = mynew.rSize()*1.75
-        ship_upgrades.upgradeUnit(mynew,diffic)
-        vec=(vec[0]-rsize,
-            vec[1],#-rsize
-            vec[2]-rsize)
+    else:  # only upgrade blank ships (for now)
+        debug.info("Launching blank ship(s)")
+        rsize=0.0
+        diffic = VS.GetDifficulty()
+        #debug.debug("VS.Unit #1")
+        ret=VS.Unit()
+        for i in range(nr_ships):
+            mynew=VS.launch(fgname,type,faction,"unit",ai,1,nr_waves,VS.SafeEntrancePoint (vec,40),logo)
+            if (i==0):
+                ret = mynew
+                rsize = mynew.rSize()*1.75
+            debug.debug("upgradeUnit '%s' using difficulty %.2f ..." % (mynew.getName(), float(diffic)))
+            ship_upgrades.upgradeUnit(mynew,diffic)
+            debug.debug("'- Done upgrading unit '%s' using difficulty %.2f ..." % (mynew.getName(), float(diffic)))
+            vec=(vec[0]-rsize,
+                vec[1],#-rsize
+                vec[2]-rsize)
     dj_lib.PlayMusik(0,dj_lib.HOSTILE_NEWLAUNCH_DISTANCE)
     return ret
-  
+
 def launch_waves_around_area(fgname,faction,type,ai,nr_ships,nr_waves,r1,r2,pos,logo='',useani=1):
     pos=((pos[0]+vsrandom.uniform(r1,r2)*vsrandom.randrange(-1,2,2)),
         (pos[1]+vsrandom.uniform(r1,r2)*vsrandom.randrange(-1,2,2)),
         (pos[2]+vsrandom.uniform(r1,r2)*vsrandom.randrange(-1,2,2)))
     return launch(fgname,faction,type,ai,nr_ships,nr_waves,pos,logo,useani)
-  
+
 def launch_wave_around_area(fgname,faction,type,ai,nr_ships,r1,r2,pos,logo='',useani=1):
-#  print 'log' + str(logo)
+    #debug.debug('logo:' + str(logo))
     return launch_waves_around_area (fgname,faction,type,ai,nr_ships,1,r1,r2,pos,logo,useani)
-  
+
 def launch_around_station(station_name,fgname,faction,type,ai,nr_ships,nr_waves,logo='',useani=1):
     station_unit=unit.getUnitByFgID(station_name)
     if(station_unit.isNull()):
         sys.stderr.write("launch.py:launch_around_station did not find unit %s\n" % (station_name))
+        #debug.debug("VS.Unit() #2")
         return VS.Unit()
     station_pos=station_unit.Position()
     rsize=station_unit.rSize()
     launched =launch_waves_around_area(fgname,faction,type,ai,nr_ships,nr_waves,rsize,rsize*2.0,station_pos,logo,useani)
     return launched
-  
+
 launch_around_unit=launch_around_station
 
 def launch_waves_in_area(fgname,faction,type,ai,nr_ships,nr_waves,radius,pos,logo='',useani=1):
@@ -63,17 +81,17 @@ def launch_waves_in_area(fgname,faction,type,ai,nr_ships,nr_waves,radius,pos,log
         pos[1]+vsrandom.uniform((-radius)/2,radius/2.0),
         pos[2]+vsrandom.uniform((-radius)/2,radius/2.0))
     un = launch(fgname,faction,type,ai,nr_ships,nr_waves,pos,logo,useani)
-  
+
 def launch_wave_in_area(fgname,faction,type,ai,nr_ships,radius,pos,logo='',useani=1):
     launch_waves_in_area(fgname,faction,type,ai,nr_ships,1,radius,pos,logo,useani)
-  
+
 def launchShipsAtWaypoints(waypoints,faction,type,ainame,nr,logo='',useani=1):
     i=0
     for wp in waypoints:
         outstr="wp%d" % (i)
         launch(outstr,faction,type,ainame,nr,1,wp,logo,useani)
         i+=1
-    
+
 def launch_wave_around_unit (fgname, faction, type, ai, nr_ships, minradius, maxradius, my_unit,logo='',useani=1):
     import faction_ships
     myvec = (0,0,0)
@@ -81,7 +99,7 @@ def launch_wave_around_unit (fgname, faction, type, ai, nr_ships, minradius, max
         un=launch_wave_around_area (fgname,faction,type,ai,nr_ships,minradius,maxradius,myvec,logo,useani)
         return un
     myvec=my_unit.LocalPosition()
-    print myvec
+    debug.info("myvec: %s" % (str(myvec)))
     rsiz=my_unit.rSize()
     if (maxradius > faction_ships.max_radius):
         maxradius=faction_ships.max_radius
@@ -89,14 +107,15 @@ def launch_wave_around_unit (fgname, faction, type, ai, nr_ships, minradius, max
         minradius=faction_ships.max_radius
     un=launch_wave_around_area (fgname,faction,type,ai,nr_ships,rsiz+minradius,rsiz+maxradius,myvec,logo,useani)
     return un
-  
+
 def launch_wave_around_significant (fgname,faction,type,ai,nr_ships,minradius, maxradius,significant_number,logo='',useani=1):
     significant_unit=unit.getSignificant(significant_number,0,0)
     if (significant_unit.isNull()):
         significant_unit = VS.getPlayer()
     launched = launch_wave_around_unit(fgname,faction,type,ai,nr_ships,minradius,maxradius,significant_unit,logo,useani)
     return launched
-  
+
+
 class Launch:
     def __init__ (self):
         self.fg='Shadow'
@@ -116,7 +135,9 @@ class Launch:
         self.fgappend=''
         self.forcetype=False;
         self.capitalp=0
+
     def Preprocess (self):
+        debug.debug("Entering Preprocess()")
         self._preprocess=1
         self._dyn_nr_ships=[]
         self._nr_ships=self.num
@@ -124,7 +145,7 @@ class Launch:
         if self.dynfg != '':
             import fg_util
             tn=fg_util.ShipsInFG(self.dynfg,self.faction)
-            print 'Dynamically launching from SaveString flightgroup '+self.dynfg + ' with ships: '+str(tn)+' faction '+ self.faction
+            debug.info('Dynamically launching from SaveString flightgroup '+self.dynfg + ' with ships: '+str(tn)+' faction '+ self.faction)
             knum=0
             if (tn!=[] and self.type!=''):
                 for i in range (len(tn)):
@@ -139,7 +160,7 @@ class Launch:
 ##          print 'Dyn-Launch: tn==[]'
 ##          self.dynfg=''
             elif (tn==[]):
-                print "Dyn-Launch: tn=="+str(tn)+", dynfg=='"+str(self.dynfg)+"' getting random fighter"
+                debug.info("Dyn-Launch: tn=="+str(tn)+", dynfg=='"+str(self.dynfg)+"' getting random fighter")
                 self.type=faction_ships.getRandomFighterInt(faction_ships.factionToInt(self.faction))
                 self.fg = self.dynfg
                 self.dynfg=''
@@ -155,16 +176,17 @@ class Launch:
                     self._dyn_nr_ships+=[i]
                     knum+=i[1]
             self._nr_ships=self.num-knum
+
     def launch(self,myunit):
         self.Preprocess()
         if (self.dynfg != ''):
-            print 'dynamic launch'
+            debug.info('dynamic launch')
             if (self._nr_ships>0):
                 if (not myunit):
-                    print 'launch area'
+                    debug.info('launch area')
                     lame= launch_wave_around_area (self.dynfg+self.fgappend, self.faction, self.type, self.ai, self._nr_ships, self.minradius, self.maxradius, self.pos, self.logo, self.useani)
                 else:
-                    print 'launch more ships'
+                    debug.info('launch more ships')
                     lame= launch_wave_around_unit (self.dynfg+self.fgappend, self.faction, self.type, self.ai, self._nr_ships, self.minradius, self.maxradius, myunit, self.logo, self.useani)
             import launch_recycle
             ret= launch_recycle.launch_types_around (self.dynfg, self.faction, self._dyn_nr_ships, self.ai, self.minradius*.5+self.maxradius*.5, myunit, 100000+self.maxradius, self.logo, self.fgappend)
@@ -174,12 +196,12 @@ class Launch:
                 return lame
         else:
             if ((not myunit) and self._nr_ships>0):
-                print 'launch area'
+                debug.info('launch area')
                 return launch_wave_around_area (self.fg+self.fgappend, self.faction, self.type, self.ai, self._nr_ships, self.minradius, self.maxradius, self.pos, self.logo, self.useani)
             elif (self._nr_ships>0):
-                print 'launch more ships'
+                debug.info('launch more ships')
                 return launch_wave_around_unit (self.fg+self.fgappend, self.faction, self.type, self.ai, self._nr_ships, self.minradius, self.maxradius, myunit, self.logo, self.useani)
             else:
-                print 'zero or negative number of ships, launching one'
+                debug.info('zero or negative number of ships, launching one')
                 return launch_wave_around_unit (self.fg+self.fgappend, self.faction, self.type, self.ai, 1, self.minradius, self.maxradius, myunit, self.logo, self.useani)
         return un
