c07b4d843604dc6b502490b71a684083eee633c1
Rune Morling <ermo.vega-strike.org@spammesenseless.net>
2015-04-02 19:57:41 +0200

    Mission scripts whitespace and debugging tweaks.

--- modules/missions/ambush.py
+++ modules/missions/ambush.py
@@ -3,140 +3,147 @@ import Director
 import debug
 import directions_mission
 
+debug.debug("importing ambush.py")
+
 class ambush(directions_mission.directions_mission):
     def privateSetupPlayer(self):
-        self.timer=0
-        self.inescapable=0
-        self.havelaunched=0
-        self.terminated=0
+        self.timer = 0
+        self.inescapable = 0
+        self.havelaunched = 0
+        self.terminated = 0
 
     def __init__(self, savevar, systems, delay, faction, numenemies, dyntype='', fgname='', greetingText=["Hello there, smuggler. Prepare to die!", "The price on your head is big enough that I missed my lunch."], directions=[], destination='', AdjustFaction=True):
         directions_mission.directions_mission.__init__ (self, savevar, directions, destination)
         debug.info('Ambush: Starting')
-        if type(faction)!=tuple and type(faction)!=list:
+        if type(faction) != tuple and type(faction) != list:
             self.faction=(faction,)
         else:
             self.faction=faction
-        if type(systems)!=tuple and type(systems)!=list:
+        if type(systems) != tuple and type(systems) != list:
             self.systems=(systems,)
         else:
             self.systems=systems
-        if type(numenemies)!=tuple and type(numenemies)!=list:
+        if type(numenemies) != tuple and type(numenemies) != list:
             self.numenemies=(numenemies,)
         else:
             self.numenemies=numenemies
-        if type(dyntype)!=tuple and type(dyntype)!=list:
+        if type(dyntype) != tuple and type(dyntype) != list:
             self.dyntype=(dyntype,)
         else:
             self.dyntype=dyntype
-        if type(fgname)!=tuple and type(fgname)!=list:
+        if type(fgname) != tuple and type(fgname) != list:
             self.fgname=(fgname,)
         else:
             self.fgname=fgname
-        self.greetingText=greetingText
-        self.cp=VS.getCurrentPlayer()
-        self.delay=delay
+        self.greetingText = greetingText
+        self.cp = VS.getCurrentPlayer()
+        debug.debug("Ambush: self.cp = %s" % (self.cp))
+        self.delay = delay
         self.privateSetupPlayer()
-        if type(AdjustFaction)!=tuple and type(AdjustFaction)!=list:
-            self.AdjustFaction=(AdjustFaction,)
+        if type(AdjustFaction) != tuple and type(AdjustFaction) != list:
+            self.AdjustFaction = (AdjustFaction,)
         else:
-            self.AdjustFaction=AdjustFaction
-    def setupPlayer(self,cp):
-        debug.info("ambush setting player up")
-        directions_mission.directions_mission.setupPlayer(self,cp)
+            self.AdjustFaction = AdjustFaction
+
+    def setupPlayer(self, cp):
+        debug.info("Ambush: setupPlayer")
+        directions_mission.directions_mission.setupPlayer(self, cp)
         self.privateSetupPlayer()
 
-    def Launch(self,you):
-        if (self.havelaunched==0):
+    def Launch(self, you):
+        if (self.havelaunched == 0):
             for i in range (len(self.faction)):
-                faction=self.faction[i]
+                faction = self.faction[i]
                 if len(self.numenemies) > i:
-                    numenemies=self.numenemies[i]
+                    numenemies = self.numenemies[i]
                 else:
-                    numenemies=self.numenemies[0]
+                    numenemies = self.numenemies[0]
                 if type(self.AdjustFaction) != bool:
                     try:
                         if len(self.AdjustFaction) > i:
-                            self.AdjustFaction=self.AdjustFaction[i]
+                            self.AdjustFaction = self.AdjustFaction[i]
                         else:
-                            self.AdjustFaction=self.AdjustFaction[0]
+                            self.AdjustFaction = self.AdjustFaction[0]
                     except:
                         AjdustFaction = True
                 if len(self.fgname) > i:
-                    fgname=self.fgname[i]
+                    fgname = self.fgname[i]
                 else:
-                    fgname=self.fgname[0]
-                if fgname=="":
+                    fgname = self.fgname[0]
+                if fgname == "":
                     fgname="Shadow"
                 if len(self.dyntype) > i:
-                    dyntype=self.dyntype[i]
+                    dyntype = self.dyntype[i]
                 else:
-                    dyntype=self.dyntype[0]
+                    dyntype = self.dyntype[0]
                 for z in range(numenemies):
                     debug.info('Ambush: Launch ships!')
-                    self.havelaunched=1
+                    self.havelaunched = 1
                     import launch
-                    L=launch.Launch()
-                    L.fg=fgname
-                    L.dynfg="" # dynfg tells launch to load flightgroup from SaveString
-                    if (dyntype==""):
+                    L = launch.Launch()
+                    L.fg = fgname
+                    L.dynfg = "" # dynfg tells launch to load flightgroup from SaveString
+                    if dyntype == "":
                         import faction_ships
-                        dyntype=faction_ships.getRandomFighter(faction)
-                    L.type=dyntype
-                    L.num=1
+                        dyntype = faction_ships.getRandomFighter(faction)
+                    L.type = dyntype
+                    L.num = 1
                     if z == 0:
-                        L.fgappend=""
+                        L.fgappend = ""
                     else:
-                        L.fgappend="_"+str(z)
-                    L.minradius=6000
-                    L.maxradius=8000
+                        L.fgappend = "_"+str(z)
+                    L.minradius = 6000
+                    L.maxradius = 8000
                     try:
                         import faction_ships
-                        L.minradius*=faction_ships.launch_distance_factor
-                        L.maxradius*=faction_ships.launch_distance_factor
+                        L.minradius *= faction_ships.launch_distance_factor
+                        L.maxradius *= faction_ships.launch_distance_factor
                     except:
                         pass
-                    L.faction=faction
+                    L.faction = faction
                     import universe     
-                    enemy=L.launch(you)
-                    lead=enemy.getFlightgroupLeader()
+                    enemy = L.launch(you)
+                    lead = enemy.getFlightgroupLeader()
                     enemy.SetTarget(you)
-                    if (lead):
+                    if lead:
                         lead.SetTarget(you)
                     else:
                         enemy.setFlightgroupLeader(enemy)
                     enemy.setFgDirective("A.")
-                    self.enemy=lead         
-                    rel=VS.GetRelation(faction,"privateer")
-                    if (self.AdjustFaction and rel>=0):
-                        VS.AdjustRelation(faction,"privateer",-.02-rel,1.0)
-                        rel=VS.GetRelation("privateer",faction)
-                        VS.AdjustRelation("privateer",faction,-.02-rel,1.0)
-                    if (i==0 and z==0):
-                        universe.greet(self.greetingText,enemy,you)
+                    self.enemy = lead
+                    rel = VS.GetRelation(faction, "privateer")
+                    if self.AdjustFaction and rel >= 0:
+                        VS.AdjustRelation(faction, "privateer", -.02 - rel, 1.0)
+                        rel = VS.GetRelation("privateer", faction)
+                        VS.AdjustRelation("privateer", faction, -.02 - rel, 1.0)
+                    if i == 0 and z==0:
+                        universe.greet(self.greetingText, enemy, you)
                     debug.info('Ambush: Ships have been launched. Exiting...')
+
     def terminate(self):
-        self.terminated=1#VS.terminateMission(0)
+        self.terminated = 1  # VS.terminateMission(0)
+
     def Execute(self):
+        #debug.debug("Ambush Execute: you = VS.getPlayerX(self.cp=%s) = %s" % (self.cp, VS.getPlayerX(self.cp)))
         directions_mission.directions_mission.Execute(self)
-        if (self.terminated==1):
+        if self.terminated == 1:
             return
-        you=VS.getPlayerX(self.cp)
+        you = VS.getPlayerX(self.cp)
         if you.isNull():
+            debug.info("Ambush: self.you is null?!")
             return
-        sys=you.getUnitSystemFile()
-        if(not self.inescapable):
+        sys = you.getUnitSystemFile()
+        if not self.inescapable:
             for i in self.systems:
-                where=sys.find(i)
-                if (where>0):
-                    if (sys[where-1]=='/'):
-                        where=0
-                if (where==0):
-                    #debug.debug('Ambush: wait before launching ship...')
-                    self.inescapable=1
-                    self.timer=VS.GetGameTime()
-        if (self.inescapable and ((self.delay==0) or (VS.GetGameTime()-self.timer>=self.delay))):
+                where = sys.find(i)
+                if where > 0:
+                    if sys[where-1] == '/':
+                        where = 0
+                if where == 0:
+                    debug.debug('Ambush: wait before launching ship...')
+                    self.inescapable = 1
+                    self.timer = VS.GetGameTime()
+        if self.inescapable and (self.delay == 0 or VS.GetGameTime() - self.timer >= self.delay):
             self.Launch(you)
             self.terminate()
             # debug.debug("it's unavoidable, my young apprentice... in "+str(self.delay)+" seconds from "+str(self.timer))
-            
--- modules/missions/directions_mission.py
+++ modules/missions/directions_mission.py
@@ -25,117 +25,114 @@ class go_none:
 isambushrunning={}
 
 class directions_mission (Director.Mission):
-    def dir_privateSetupPlayer(self,cp):
+    def dir_privateSetupPlayer(self, cp):
         debug.info("setting up mission")
-        debug.info("jumps:"+self.jumps)
-        self.arrived=0
-        self.wasnull=0
-        self.you=VS.getPlayerX(cp)
-        self.adjsys=go_to_adjacent_systems(self.you,0,self.jumps)
+        debug.info("jumps: %s" % (str(self.jumps)))
+        self.arrived = 0
+        self.wasnull = 0
+        self.you = VS.getPlayerX(cp)
+        self.adjsys = go_to_adjacent_systems(self.you, 0, self.jumps)
         self.adjsys.Print("You should start in the system named %s","Then jump to %s","Lastly, jump to %s, your final destination","cargo mission",1)
 
-    def setupPlayer(self,cp):
+    def setupPlayer(self, cp):
         self.dir_privateSetupPlayer(cp)
 
-    def __init__ (self,savevar,jumps=(),destination=''):
+    def __init__(self, savevar, jumps=(), destination=''):
         Director.Mission.__init__(self);
-        debug.info("Directions: Starting")
+        debug.info("Directions Mission: Starting")
         global isambushrunning
-        self.var=savevar
-        self.savedCargo=self.getCargo(VS.getPlayer())
-        debug.debug("self.savedCargo:"+self.savedCargo)
-        if (self.var,self.savedCargo) in isambushrunning:
+        self.var = savevar
+        self.savedCargo = self.getCargo(VS.getPlayer())
+        debug.debug("self.savedCargo: %s" % (str(self.savedCargo)))
+        if (self.var, self.savedCargo) in isambushrunning:
             #VS.terminateMission(0)
-            debug.info("Directions: Stopping: directions already running! (before mission restore)")
-        isambushrunning[(self.var,self.savedCargo)]=True
-        self.jumps=jumps
-        self.cp=VS.getCurrentPlayer()
-        self.you=VS.Unit()
-        self.base=VS.Unit()
-        self.arrived=0
-        self.destination=destination
-        self.mplay="all"
+            debug.info("Directions Mission: Stopping: directions already running! (before mission restore)")
+        isambushrunning[(self.var, self.savedCargo)] = True
+        self.jumps = jumps
+        debug.debug("Directions Mission: self.cp = %s" %(VS.getCurrentPlayer()))
+        self.cp = VS.getCurrentPlayer()
+        self.you = VS.Unit()
+        self.base = VS.Unit()
+        self.arrived = 0
+        self.destination = destination
+        self.mplay = "all"
         self.dir_privateSetupPlayer(self.cp)
-        self.mplay=universe.getMessagePlayer(self.you)
-        self.obj=0
-        name = self.you.getName ()
+        self.mplay = universe.getMessagePlayer(self.you)
+        self.obj = 0
+        name = self.you.getName()
 
-    def takeCargoAndTerminate (self,you, remove=1):
+    def takeCargoAndTerminate(self,you, remove=1):
         global isambushrunning
-        if ((self.var,self.savedCargo) in isambushrunning):
-            del isambushrunning[(self.var,self.savedCargo)]
-        if self.var!='':
-            quest.removeQuest(self.you.isPlayerStarship(),self.var,1)
+        if (self.var, self.savedCargo) in isambushrunning:
+            del isambushrunning[(self.var, self.savedCargo)]
+        if self.var:
+            quest.removeQuest(self.you.isPlayerStarship(), self.var, 1)
         VS.terminateMission(1)
         return
         
     def findUnit(self, name):
-        debug.debug("testun=VS.getUnit(0)")
-        testun=VS.getUnit(0)
-        itt=1
+        debug.debug("testun = VS.getUnit(0)")
+        testun = VS.getUnit(0)
+        itt = 1
         while(not testun.isNull()):
             if testun.getName().lower()==name.lower() or testun.getFullname().lower()==name.lower():
                 return testun
-            debug.debug("testun=VS.getUnit(%d)" % (itt))
-            testun=VS.getUnit(itt)
-            itt+=1
-        debug.debug("testun=VS.getUnit(0)")
-        testun=VS.getUnit(0)
+            debug.debug("testun = VS.getUnit(%d)" % (itt))
+            testun = VS.getUnit(itt)
+            itt += 1
+        debug.debug("testun = VS.getUnit(0)")
+        testun = VS.getUnit(0)
         while(not testun.isNull()):
             if testun.isDockableUnit():
                 return testun
-            debug.debug("testun=VS.getUnit(%d)" % (itt))
-            testun=VS.getUnit(itt)
-            itt+=1
+            debug.debug("testun = VS.getUnit(%d)" % (itt))
+            testun = VS.getUnit(itt)
+            itt += 1
         debug.debug("return VS.getUnit(0)")
         return VS.getUnit(0)
 
     def getCargo(self,un):
-        lis=[]
+        lis = []
         for i in range(un.numCargo()):
-            if (un.GetCargoIndex(i).GetMissionFlag()):
+            if un.GetCargoIndex(i).GetMissionFlag():
                 lis.append(un.GetCargoIndex(i).GetContent())
         return tuple(lis)
 
     def checkCargo(self,un):
-        return not (quest.checkSaveValue(self.cp,self.var,0) or quest.checkSaveValue(self.cp,self.var,1) or quest.checkSaveValue(self.cp,self.var,-1) or self.getCargo(un)!=self.savedCargo)
+        return not (quest.checkSaveValue(self.cp, self.var, 0) or quest.checkSaveValue(self.cp, self.var, 1) or quest.checkSaveValue(self.cp, self.var, -1) or self.getCargo(un)!=self.savedCargo)
 
-    def Execute (self):
-        if (VS.getPlayerX(self.cp).isNull()):
-            self.wasnull=1
+    def Execute(self):
+        #debug.debug("Directions Mission Execute: you = VS.getPlayerX(self.cp=%s) = %s" % (self.cp, VS.getPlayerX(self.cp)))
+        you = VS.getPlayerX(self.cp)
+        if you.isNull():
+            self.wasnull = 1
             return
-        if (self.arrived and self.base.isNull()):
+        if self.arrived and self.base.isNull():
             return
-        if (self.wasnull):
+        if self.wasnull:
             debug.info("INEQUALITY")
             if (not self.checkCargo(VS.getPlayerX(self.cp))):
-                self.takeCargoAndTerminate(self.you,1)
+                self.takeCargoAndTerminate(self.you, 1)
                 return
             else:
                 self.setupPlayer(self.cp)
-        if (not self.you):
+        if not self.you:
             return
-        if (not self.adjsys.Execute()):
+        if not self.adjsys.Execute():
             return
-        if (self.destination=='' and len(self.jumps)==0):
-            return #obviously wrapper for ambush
-        if (self.arrived):
-            dis=self.you.getSignificantDistance(self.base)
-            if (dis<200):
-                VS.setCompleteness(self.obj,1)
-            if (dis<50 or self.base.isDocked(self.you) or self.you.isDocked(self.base)):
-                self.takeCargoAndTerminate(self.you,1)
+        if self.destination=='' and len(self.jumps)==0:
+            return  # obviously wrapper for ambush
+        if self.arrived:
+            dis = self.you.getSignificantDistance(self.base)
+            if dis < 200:
+                VS.setCompleteness(self.obj, 1)
+            if dis < 50 or self.base.isDocked(self.you) or self.you.isDocked(self.base):
+                self.takeCargoAndTerminate(self.you, 1)
                 return
         else:
-            self.arrived=1
-            self.adjsys=go_none()
-            self.base=self.findUnit(self.destination)
-            self.obj=VS.addObjective("Deliver cargo to %s." % self.destination);
-            VS.setOwner(self.obj,self.you)
-            VS.setCompleteness(self.obj,0)
-
-
-
-
-
-
+            self.arrived = 1
+            self.adjsys = go_none()
+            self.base = self.findUnit(self.destination)
+            self.obj = VS.addObjective("Deliver cargo to %s." % self.destination);
+            VS.setOwner(self.obj, self.you)
+            VS.setCompleteness(self.obj, 0)
